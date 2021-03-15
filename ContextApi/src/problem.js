3be13d9717f5a93f779ad8289cb0f4f85f463348
pradeep kumar <pradeeakumar@gmail.com>
2021-03-15 12:31:09 +0530

    done

---
+++ ContextApi/src/problem.js
@@ -0,0 +1,26 @@
+// import React,{useState} from "react";
+
+// const GrandChild=(props)=>{
+//   return(
+//     <div>
+//         <h3>GrandChild :</h3>
+//         <Child brand1={props.brand}/>
+//     </div>   
+//   );
+// }
+// const Child = props=>(
+//   <div>
+//       <h2>Child: {props.brand1}</h2>
+//   </div>
+// )
+
+// const App=()=>{
+//   const [brandname] = useState("Aamazon!");
+//   return( 
+//     <div>
+//       <h1>HELLO WORLD!</h1>      
+//       <GrandChild brand={brandname}/>              
+//     </div>
+//   );
+// }
+// export default App;
