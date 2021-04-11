3b80e5ebcd233895056fe3258e1c150a427ace4b
Pradeep Kumar <48400035+pradeea@users.noreply.github.com>
2021-04-12 01:06:00 +0530

    Create api-module-file.js

---
+++ Pics project/api-module-file.js
@@ -0,0 +1,22 @@
+//this is an new code in the api module
+import axios from "axios";
+
+const KEY='AIzaSyCxgU-BXEwud5uf5YeeQRM4tdN_VO5Q40s';
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+    params: {
+            part:'snippet',
+            maxResults:5,
+            key:KEY
+
+    }
+});
+
+//after this u need to write the followig code in the app.js
+onTermSubmit=(term)=>{
+        youtube.get('/search',{
+            params:{
+                q:term
+            }
+        })
+    }
