590126c3284f9d4feb6cb23744e6cae45659d0ee
Pradeep Kumar <48400035+pradeea@users.noreply.github.com>
2021-05-18 14:17:58 +0530

    Create App.js

---
+++ Under Maintainence/New folder/App.js
@@ -0,0 +1,12 @@
+import Display from "./components/Display";
+import Login from "./components/Login";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Display /> */}
+      <Login />
+    </div>
+  );
+}
